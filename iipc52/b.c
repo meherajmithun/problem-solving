509548a7a56af5962643678f193f4f388bb5694c
meherajmithun <meherajhossainmithun@gmail.com>
2024-07-07 01:43:49 +0600

    updated

--- iipc52/b.c
+++ iipc52/b.c
@@ -1,12 +1,20 @@
 #include<stdio.h>
+#include<string.h>
 int main(){
-    int a,b;
+    int a,i,b;
+    char str[100];
+    scanf("%s",&str);
     scanf("%d %d",&a,&b);
-    if(a<=b){
-        printf("unsafe");
+    int x = strlen(str);
+    for( i = 0 ; i < x ; i++){
+        if(i == a-1){
+            printf("%c",str[b-1]);
+        }
+        else if(i == b-1 ){
+            printf("%c",str[a-1]);
+        }
+        else {
+            printf("%c",str[i]);
+        }
     }
-    else {
-        printf("safe");
-    }
-    return 0;
 }

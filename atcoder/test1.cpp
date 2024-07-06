1678b29a6f9884d1144e4d3e20c6190659e0a7b3
meherajmithun <meherajhossainmithun@gmail.com>
2024-07-07 01:40:50 +0600

    updated code

--- atcoder/test1.cpp
+++ atcoder/test1.cpp
@@ -1,9 +1,8 @@
 #include<iostream>
 using namespace std;
 int main(){
-	int a,b,c;
-	cin>>a>>b>>c;
-	string s;
-	cin>>s;
-	cout<<a+b+c<<" "<<s;
+	int a,b;
+	cin>>a>>b;
+	if((a*b) % 2 ==0) cout<<"Even"<<endl;
+	else cout<<"Odd"<<endl;
 }

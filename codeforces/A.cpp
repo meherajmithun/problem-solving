500df2df3d984dcb6c650cb250939c0d9e4bc467
meherajmithun <meherajhossainmithun@gmail.com>
2024-07-08 19:10:45 +0600

    updated

--- codeforces/A.cpp
+++ codeforces/A.cpp
@@ -3,9 +3,11 @@ using namespace std;
 int main(){
 	int t;
 	cin>>t;
-	int n;
-	cin>>n;
-	string s;
-	cin>>s;
-	cout<<(n == 1 || (n == 2 and s[0] != s[1]) ? "YES" : "NO")<<endl;
+	while(t--){
+		int n;
+		cin>>n;
+		string s;
+		cin>>s;
+		cout<<(n == 1 || (n == 2 and s[0] != s[1]) ? "YES" : "NO")<<endl;
+	}
 }

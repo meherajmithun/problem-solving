3c871c1b3db15716d3d2c428347d5285a355bbdf
meherajmithun <meherajhossainmithun@gmail.com>
2024-07-14 01:01:15 +0600

    solved

--- iipc52/I.cpp
+++ iipc52/I.cpp
@@ -1,17 +1,17 @@
-#include<iostream>
-#include<cmath>
+/*
+  IN THE NAME OF ALLAH
+  Author: Meheraj Mithun
+  Bangladesh University Of Businness and Teachnology
+*/
+
+#include<bits/stdc++.h>
 using namespace std;
-int main(){
-    double a,b,c,d;
-    cin>>a>>b>>c>>d;
-    double x = (c - a);
-    x = (x*x);
-    double y = (d - b);
-    y = (y*y);
 
-    double result = sqrt(x+y);
-
-    cout<<result;
-
-
+int main() {
+    double x1, y1, x2, y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+    double distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    cout << fixed << setprecision(9) << distance << endl;
+    return 0;
 }
+

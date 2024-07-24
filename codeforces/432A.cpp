0a5c032be55a7766cc3143c8f4fb6e8f23cee6d0
meherajmithun <meherajhossainmithun@gmail.com>
2024-07-24 23:11:37 +0600

    solved

---
+++ codeforces/432A.cpp
@@ -0,0 +1,21 @@
+/*
+  IN THE NAME OF ALLAH
+  Author: Meheraj Mithun
+  Bangladesh University Of Businness and Teachnology
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n,a,count=0;
+	cin>>n>>a;
+	int arr[n];
+	for(int i = 0; i < n; i++){
+		cin>>arr[i];
+	}
+	for(int i = 0; i < n; i++){
+		if(arr[i] + a <= 5) count++;
+	}
+	cout<< count/3<<endl;
+}

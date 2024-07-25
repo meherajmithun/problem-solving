acaeac99668699e2f9db78e0ea597adc08bf79f4
meherajmithun <meherajhossainmithun@gmail.com>
2024-07-25 15:05:10 +0600

    solved

--- codeforces/1807B.cpp
+++ codeforces/1807B.cpp
@@ -4,30 +4,32 @@
   Bangladesh University Of Businness and Teachnology
 */
 
-#include<iostream>
-#include<vector>
+#include<bits/stdc++.h>
 using namespace std;
 
-void loser(){
-	int n,i,even=0,odd=0;
-	cin>>n;
-	vector<int>candies(n);
-	for(i = 0 ; i < n ; i++){
-		cin>>candies[i];
-		if(candies[i] % 2 == 0){
-			even++;
-		}
-		else {
-			odd++;
-		}
-	}
-	if(even >= odd) cout<<"YES"<<endl;
-	else cout<<"NO"<<endl;
+string solve(){
+    int n,i,even=0,odd=0;
+    cin>>n;
+    int arr[n],sum=0;
+    for(i = 0; i < n; i++){
+        cin>>arr[i];
+    }
+
+    if(arr[n-1] % 2 == 0) even = even + arr[n-1];
+    else if (arr[n-1] % 2 != 0) odd = odd + arr[n-1];
+
+    for(i = 0; i < n-1; i++){
+        if(arr[i] % 2 ==0) even = even + arr[i];
+        else odd = odd + arr[i];
+    }
+	
+    if(even > odd) return "YES";
+    else return "NO";
 }
 int main(){
-	int t;
-	cin>>t;
-	while(t--){
-		loser();
-	}
+    int T;
+    cin>>T;
+    while(T--){
+        cout<<solve() <<endl;
+    }
 }

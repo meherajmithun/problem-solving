3552a9e48c288458a04f7c4b43c3f1ddbbca7b96
meherajmithun <meherajhossainmithun@gmail.com>
2024-08-23 22:38:55 +0600

    solved

--- codeforces/228A.cpp
+++ codeforces/228A.cpp
@@ -1,1 +1,21 @@
+/*
+  IN THE NAME OF ALLAH
+  Author: Meheraj Mithun
+  Bangladesh University Of Businness and Teachnology
+*/
+#include<bits/stdc++.h>
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl '\n'
+using namespace std;
+
+int main(){
+	optimize();
+	vector<int>v(4);
+	for(int i = 0; i < 4; i++) cin >> v[i];
+	sort(v.begin() , v.end());
+
+	int sz = unique(v.begin() , v.end()) - v.begin();
+	cout<<4-sz<<endl;
+}
+
 

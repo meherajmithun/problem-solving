415223610bb813ce3f23d1640ca3457765ded438
meherajmithun <meherajhossainmithun@gmail.com>
2024-08-26 01:40:37 +0600

    solved

--- codeforces/1551A.cpp
+++ codeforces/1551A.cpp
@@ -1,34 +1,26 @@
 /*
   IN THE NAME OF ALLAH
   Author: Meheraj Mithun
-  Bangladesh University Of Businness and Teachnology
+  Bangladesh University Of Business and Technology
 */
-
 #include<bits/stdc++.h>
+#define fastIO ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl '\n'
 using namespace std;
 
 void solve(){
-    long long i=0,n;
-    cin>>n;
-    while(i != n){
-        if(i * 1 + i * 2 == n){
-            cout<<i <<" " <<i<<endl;
-            break;
-        }
-        else if((i+1 * 1) + i * 2 == n){
-            cout<<i+1 <<" " <<i<<endl;
-            break;
-        }
-        else if((i * 1) + ((i + 1) * 2) == n){
-            cout<<i <<" "<<i+1<<endl;
-        }
-        i++;
-    }
+    int n; cin >> n;
+    int cnt_x = n / 3;
+    int cnt_y = n / 3;
+    if(n % 3 == 1) cnt_x++;
+    else if(n % 3 == 2) cnt_y++;
+    cout<<cnt_x<<" "<<cnt_y<<endl;
 }
 int main(){
+    fastIO;
     int T;
-    cin>>T;
+    cin >> T;
     while(T--){
-        solve();
+         solve() ;
     }
 }

d1d1d202f2efb24ecb1a28f0329a15b3e9b41e89
meherajmithun <meherajhossainmithun@gmail.com>
2024-08-26 17:21:52 +0600

    solved

--- codeforces/A_Odd_Divisor.cpp
+++ codeforces/A_Odd_Divisor.cpp
@@ -4,37 +4,27 @@
   Bangladesh University Of Business and Technology
 */
 #include<bits/stdc++.h>
+using namespace std;
 #define fastIO ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define endl '\n'
 #define pb push_back
-//#define int long long
-using namespace std;
+#define ll long long
+#define no (cout << "NO\n")
+#define yes (cout << "YES\n")
 
 void solve(){
-    long long n; cin >> n;
-    long long count = 0;
-    if(n == 2){
-        cout<<"NO"<<endl;
-        return ;
-    }
-    else {
-        for(long long i = 1; i * i <= n; i++){
-            if(n % i == 0){
-                count++;
-                if(i != n / i) count++;
-            }
-        }
-    }
-    count -= 1;
-    if(count % 2 != 0) cout<<"YES" <<endl;
-    else cout<<"NO"<<endl;
+    ll n;
+    cin >> n;
+    if(n & (n-1)) yes;
+    else no;
 }
+
 int main(){
     fastIO;
     int T;
     cin >> T;
     while(T--){
-         solve() ;
+         solve();
     }
     return 0;
 }

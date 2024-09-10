c2d0f78d1626edb82136688b087a9a23efbc584f
meherajmithun <meherajhossainmithun@gmail.com>
2024-09-11 02:55:44 +0600

    solved

---
+++ codeforces/A_Closing_The_Gap.cpp
@@ -0,0 +1,42 @@
+/*
+  IN THE NAME OF ALLAH
+  Author: Meheraj Mithun
+  Bangladesh University Of Business and Technology
+*/
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+#define fastIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(v) v.begin(),v.end()
+#define endl '\n'
+#define pb push_back
+#define no cout << "NO\n"
+#define yes cout << "YES\n"
+
+void solve() {
+   int n ; cin >> n;
+   int a ; int ans = 0;
+   for(int i = 0; i < n; i++){
+    cin >> a;
+    ans +=a;
+   }
+   if(ans % n == 0)cout<<0<<endl;
+   else cout<<1<<endl;
+
+}
+
+int main() {
+    fastIO;
+    int T;
+    cin >> T;
+    while (T--) {
+        solve();
+    }
+    return 0;
+}

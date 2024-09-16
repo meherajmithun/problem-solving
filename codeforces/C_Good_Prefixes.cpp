6582484b2f5b766158dabf0574dcdcbbbcc2b8b4
meherajmithun <meherajhossainmithun@gmail.com>
2024-09-16 23:58:18 +0600

    solved

--- codeforces/C_Good_Prefixes.cpp
+++ codeforces/C_Good_Prefixes.cpp
@@ -12,43 +12,35 @@ typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 typedef vector<pii> vii;
 typedef vector<pll> vll;
-#define fastIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define control ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define all(v) v.begin(),v.end()
 #define endl '\n'
 #define pb push_back
 #define no cout << "NO\n"
 #define yes cout << "YES\n"
+#define int long long
 
 void solve() {
-    ll n ; cin >>n;
+    ll n; cin >> n;
     vl v(n);
-    for(ll i = 0; i < n; i++) cin >> v[i];
+    for(int i = 0; i < n; i++) cin >> v[i];
     
-    vl v2;
-    ll cnt = 0;
+    ll cnt = 0,sum = 0;
+    ll mx = INT_MIN;
     
-    for(ll i = 0; i < 1; i++){
-        v2.push_back(v[i]);
-        if(v[i] == 0){
+    for(int i = 0; i < n; i++){
+        sum += v[i];
+        
+        mx = max(mx,v[i]);
+        if(sum - mx == mx){
             cnt++;
-        }
-        for(ll j = 1; j < n; j++){
-            v2.push_back(v[j]);
-            ll x = *max_element(v2.begin() , v2.end());
-            ll sum = 0;
-            for(ll k = 0; k < v2.size(); k++){
-                sum += v2[k];
-            }
-            if(sum - x == x){
-                cnt++;
-            }
         }
     }
     cout<<cnt<<endl;
 }
 
-int main() {
-    fastIO;
+int32_t main() {
+    control;
     int T;
     cin >> T;
     while (T--) {

8303aae171cd407173b6f5b3911c84229f1be33a
meherajmithun <meherajhossainmithun@gmail.com>
2024-09-20 20:16:39 +0600

    solved

--- codeforces/A_Contest_Proposal.cpp
+++ codeforces/A_Contest_Proposal.cpp
@@ -12,37 +12,41 @@ typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 typedef vector<pii> vii;
 typedef vector<pll> vll;
-#define fastIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define all(a) (a).begin(),(a).end()
+#define control ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(v) v.begin(),v.end()
 #define endl '\n'
 #define pb push_back
 #define no cout << "NO\n"
 #define yes cout << "YES\n"
+#define int long long
 
 void solve() {
-    int n, m;
-    cin >> n >> m;
-    int ans = 0;
-    while (n > 0 && m > 0) {
-        if (n > m) {
-
-            n -= 2;
-            m -= 1;
-        } 
-        else {
-            n -= 1;
-            m -= 2;
-        }
-        
-        if (n >= 0 && m >= 0) {
-            ans++;
-        }
+   int n ; cin >> n;
+   vl a;
+   vl b;
+   for(int i = 0; i < n; i++){
+    int x ; cin >> x;
+    a.pb( x );
+   }
+   for(int i = 0; i < n; i++){
+    int y ; cin >> y;
+    b.pb( y );
+   }
+   int cnt = 0 , j = 0;
+   for(int i = 0; i < n; i++){
+    if(a[j] > b[i]){
+        cnt++;
     }
-    cout << ans << endl;
+    else{
+        j++;
+    }
+   }
+   //cout<<endl;
+   cout<<cnt<<endl;
 }
 
-int main() {
-    fastIO;
+int32_t main() {
+    control;
     int T;
     cin >> T;
     while (T--) {

eec200e3d429d004f72bfb8547d0b3ac0ea21eac
meherajmithun <meherajhossainmithun@gmail.com>
2024-11-07 01:52:41 +0600

    solved

---
+++ AL Techemist contest/Z_Z.cpp
@@ -0,0 +1,33 @@
+//  IN THE NAME OF ALLAH
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int, int> pii;
+#define slow ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(v) v.begin(),v.end()
+#define endl '\n'
+#define pb push_back
+#define no cout << "NO\n"
+#define yes cout << "YES\n"
+#define int long long
+
+void solve() {
+    int a , b ;
+    cin >> a >> b;
+    if(a > 0 and b == 0) cout<<"Gold"<<endl;
+    else if(b > 0 and a == 0) cout<<"Silver"<<endl;
+    else cout<<"Alloy"<<endl;
+}
+
+int32_t main() {
+    slow;
+    int tc = 1;
+ //   cin >> tc;
+    while (tc--) {
+        solve();
+    }
+    return 0;
+}

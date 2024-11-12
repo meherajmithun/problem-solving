aab26b794f27559c27c7a994f0c3814e5c80d2f4
meherajmithun <meherajhossainmithun@gmail.com>
2024-11-13 02:34:33 +0600

    added

---
+++ codeforces/A_Little_Elephant_and_Rozdil.cpp
@@ -0,0 +1,42 @@
+//  IN THE NAME OF ALLAH
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int, int> pii;
+#define slow ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(v) v.begin(),v.end()
+#define endl '\n'
+#define pb push_back
+#define no cout << "NO\n"
+#define yes cout << "YES\n"
+#define int long long
+#define double long double;
+
+void solve() {
+    int n ; cin >> n;
+    vi v(n);
+    for(auto &u : v) cin >> u;
+    int mini = *min_element(all(v));
+    int cnt = count(all(v),mini);
+    //cout<<cnt<<endl;
+    if(cnt > 1){
+        cout<<"Still Rozdil"<<endl;
+        return ;
+    }
+    int ans = find(all(v),mini) - v.begin();
+    cout<<ans+1<<endl; // +1 is for 1 based
+
+}
+
+int32_t main() {
+    slow;
+    int tc = 1;
+ //   cin >> tc;
+    while (tc--) {
+        solve();
+    }
+    return 0;
+}
---
+++ codeforces/A_Parallelepiped.cpp
@@ -0,0 +1,32 @@
+//  IN THE NAME OF ALLAH
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int, int> pii;
+#define slow ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(v) v.begin(),v.end()
+#define endl '\n'
+#define pb push_back
+#define no cout << "NO\n"
+#define yes cout << "YES\n"
+#define int long long
+#define double long double;
+
+void solve() {
+    int a , b , c;
+    cin >> a >> b >> c;
+    cout<<(2)*(2)*(c+1)<<endl;
+}
+
+int32_t main() {
+    slow;
+    int tc = 1;
+ //   cin >> tc;
+    while (tc--) {
+        solve();
+    }
+    return 0;
+}

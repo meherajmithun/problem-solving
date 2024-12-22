f1aa54c1817eaf4226d933e4b8582f9cf6f39b83
meherajmithun <meherajhossainmithun@gmail.com>
2024-12-22 14:07:00 +0600

    added

---
+++ codeforces/A_Digits_Sum.cpp
@@ -0,0 +1,31 @@
+//  IN THE NAME OF ALLAH
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int, int> pii;
+#define slow ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(v) v.begin(),v.end()
+#define nl '\n'
+#define pb push_back
+#define no cout << "NO\n"
+#define yes cout << "YES\n"
+#define int long long
+#define double long double;
+
+void solve() {
+    int n ; cin >> n;
+    cout<<(n+1)/10<<endl;   
+}
+
+int32_t main() {
+    slow;
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        solve();
+    }
+    return 0;
+}

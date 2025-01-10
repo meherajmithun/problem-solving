0af78f1ec3df436ab08a3d47749978796f27310d
meherajmithun <meherajhossainmithun@gmail.com>
2025-01-10 20:50:06 +0600

    added

--- codeforces/A_Halloumi_Boxes.cpp
+++ codeforces/A_Halloumi_Boxes.cpp
@@ -1,34 +1,55 @@
-/*
-  IN THE NAME OF ALLAH
-  Author: Meheraj Mithun
-  Bangladesh University Of Business and Technology
-*/
 #include<bits/stdc++.h>
 using namespace std;
-#define fastIO ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define endl '\n'
-#define pb push_back
 #define ll long long
-#define no (cout << "NO\n")
-#define yes (cout << "YES\n")
 
+void optimize(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);cout.tie(0);
+}
 void solve(){
-    int n ; cin >> n;
-    int k ; cin >> k;
-    vector<int>v(n);
-    vector<int>v2(n);
-    for(int i = 0;i < n; i++){
-        cin >> v[i];
-        v2[i] = v[i];
-    }
+	int n,k;
+	cin >> n >> k;
+	vector<ll>v(n);
+	vector<ll>v2(n);
+	for(int i=0; i<n; i++){
+		cin >> v[i];
+		v2[i] = v[i];
+	}
+	if(n == k){
+		reverse(v2.begin() , v2.end());
+		if(is_sorted(v2.begin() , v2.end())){
+			cout<<"YES"<<endl;
+			return;
+		}
+	}
+
+	if(is_sorted(v.begin() , v.end())){
+		cout<<"YES"<<endl;
+		return;
+	}
+	int l=1,r=0;
+	while(l < n){
+		if(v[l-1] > v[l] and (l-r+1) <= k){
+			swap(v[l-1] , v[l]);
+			r = l;
+			l = 0;
+		}
+		r++;
+		l++;
+		if(is_sorted(v.begin() , v.end())){
+			cout<<"YES"<<endl;
+			return;
+		}
+	}
+	cout<<"NO"<<endl;
+}
+int main(){
+	optimize();
+	int tc; cin >> tc;
+	while(tc--){
+		solve();
+	}
 }
 
-int main(){
-    fastIO;
-    int T;
-    cin >> T;
-    while(T--){
-         solve();
-    }
-    return 0;
-}
+///2 3 1
+///2 1 3
--- lightoj/test.cpp
+++ lightoj/test.cpp
@@ -6,16 +6,50 @@ void optimize(){
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);cout.tie(0);
 }
+void solve(){
+	int n,k;
+	cin >> n >> k;
+	vector<ll>v(n);
+	vector<ll>v2(n);
+	for(int i=0; i<n; i++){
+		cin >> v[i];
+		v2[i] = v[i];
+	}
+	if(n == k){
+		reverse(v2.begin() , v2.end());
+		if(is_sorted(v2.begin() , v2.end())){
+			cout<<"YES"<<endl;
+			return;
+		}
+	}
+
+	if(is_sorted(v.begin() , v.end())){
+		cout<<"YES"<<endl;
+		return;
+	}
+	int l=1,r=0;
+	while(l < n){
+		if(v[l-1] > v[l] and (l-r+1) <= k){
+			swap(v[l-1] , v[l]);
+			r = l;
+			l = 0;
+		}
+		r++;
+		l++;
+		if(is_sorted(v.begin() , v.end())){
+			cout<<"YES"<<endl;
+			return;
+		}
+	}
+	cout<<"NO"<<endl;
+}
 int main(){
 	optimize();
-	int n; cin >> n;
-	vector<ll>v(n);
-	for(auto &u : v) cin >> u;
-	vector<ll>pre(n+1);
-	ll ans = INT_MIN,mx = INT_MIN;
-	for(int i=0; i<n; i++){
-		mx = max(v[i] , mx+v[i]);
-		ans = max(ans,mx);
+	int tc; cin >> tc;
+	while(tc--){
+		solve();
 	}
-	cout<<ans<<endl;
 }
+
+///2 3 1
+///2 1 3

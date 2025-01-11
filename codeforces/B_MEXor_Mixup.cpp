96076238e52817bbe750284403ac786e2350084f
meherajmithun <meherajhossainmithun@gmail.com>
2025-01-11 13:45:51 +0600

    added

--- codeforces/B_MEXor_Mixup.cpp
+++ codeforces/B_MEXor_Mixup.cpp
@@ -1,23 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+vector<int>v;
+void call(){
+	v.push_back(0);
+	for(int i=1; i<3e5+123; i++) v.push_back(v[i-1]^i);
+}
+
 void optimize(){
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);cout.tie(0);
 }
 
+
 void solve(){
 	int a,b; cin >> a >> b;
-	int k = b;
-	for(int i=0; i<a; i++){
-		k ^= i;
-	}
+	int k = b^v[a-1];
 	if(k == 0) cout<<a<<endl;
 	else if(k != a) cout<<(a+1)<<endl;
 	else cout<<(a+2)<<endl;
 }
 int main(){
 	optimize();
+	call();
+	
 	int tc; cin >> tc;
 	while(tc--){
 		solve();
--- lightoj/test.cpp
+++ lightoj/test.cpp
@@ -5,21 +5,19 @@ void optimize(){
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);cout.tie(0);
 }
+
 void solve(){
-	int n,k; cin >> n >> k;
-	vector<int>v(n);
-	for(auto &u : v) cin >> u;
-	
-	int mx = v[0];
-	for(int i=1; i<n; i++){
-		if(mx < (v[i]-v[i-1])){
-			mx = v[i]-v[i-1];
+	int n; cin >> n;
+	string s; cin >> s;
+	for(int i=0; i<n; i++){
+		if(s[i] == 'U'){
+			s[i] = 'D';
 		}
+        else if(s[i] == 'D'){
+            s[i] = 'U';
+        }
 	}
-	int tmp = 2*(k-v[n-1]);
-	if(mx < tmp) mx = tmp; 
-	cout<<mx<<endl;
-	//cout<<*max_element(v.begin() , v.end())<<endl;
+	cout<<s<<endl;
 }
 int main(){
 	optimize();

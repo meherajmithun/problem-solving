9e5e81e374bd24d9935d297fc6c2216a82bd60ac
meherajmithun <meherajhossainmithun@gmail.com>
2025-01-13 21:52:02 +0600

    added

--- vjudge/E_Yet_Another_String_Game.cpp
+++ vjudge/E_Yet_Another_String_Game.cpp
@@ -1,49 +1,38 @@
-//  IN THE NAME OF ALLAH
-
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-typedef vector<int> vi;
-typedef vector<ll> vl;
-typedef pair<int, int> pii;
-#define slow ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define all(v) v.begin(),v.end()
-#define endl '\n'
-#define pb push_back
-#define no cout << "NO\n"
-#define yes cout << "YES\n"
-#define int long long
-#define double long double;
+#define ll long long
 
-void solve() {
-    string s ; cin >> s;
-    for(int i = 0; i < s.size(); i++){
+void optimize(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);cout.tie(0);
+}
+
+void solve(){
+    string s; cin >> s;
+    for(int i=0; i<(int)s.size(); i++){
         if(i % 2 == 0){
-            if(s[i] == 'z'){
+            if(s[i] != 'a'){
                 s[i] = 'a';
             }
             else{
-                s[i]++;
+                s[i]++;;
             }
         }
         else{
-            if(s[i] == 'a') {
-                s[i] = 'z';
-            }
-            else {
+            if(s[i] == 'z'){
                 s[i]--;
             }
+            else{
+                s[i] = 'z';
+            }
         }
-    }   
+    }
     cout<<s<<endl;
 }
-
-int32_t main() {
-    slow;
-    int tc;
-    cin >> tc;
-    while (tc--) {
+int main(){
+	optimize();
+    int tc = 1; cin >> tc;
+    while(tc--){
         solve();
     }
-    return 0;
 }
--- vjudge/Gift_Carpet.cpp
+++ vjudge/Gift_Carpet.cpp
@@ -1,72 +1,45 @@
-//  IN THE NAME OF ALLAH
-
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-typedef vector<int> vi;
-typedef vector<ll> vl;
-typedef pair<int, int> pii;
-#define slow ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define all(v) v.begin(),v.end()
-#define endl '\n'
-#define pb push_back
-#define no cout << "NO\n"
-#define yes cout << "YES\n"
-#define int long long
-#define double long double;
+#define ll long long
 
-void solve() {
-    int n , k ;
-    cin >> n >> k;
-    string s ;
-    bool flag = true;
-    for(int i = 0; i < n; i++){
-        string a ; cin >> a;
-        string b = a;
-        sort(b.begin() , b.end());
-        int x = unique(b.begin() , b.end())-b.begin();
-        if(x == 4 and b == "aikv"){
-            flag = false;
-        }
-        else{
-            for(int j = 0; j < k; j++){
-                if(a[j] == 'v' and s.empty()){
-                    s.pb(a[j]);
-                    break;
-                }
-                else if(a[j] =='i' and s[s.size()-1] == 'v'){
-                    s.pb(a[j]);
-                    break;
-                }
-                else if(a[j] == 'k' and s[s.size()-1] == 'i'){
-                    s.pb(a[j]);
-                    break;
-                }
-                else if(a[j] == 'a' and s[s.size()-1]== 'k'){
-                    s.pb(a[j]);
-                    break;
-                }
-            }
-        }
-    }   
-    if(flag == false){
-        yes;
-        return;
-    }
-    ///
-    cout<<s<<endl;
-    if(s == "vika") {
-        yes;
-    }
-    else no;
+void optimize(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);cout.tie(0);
 }
 
-int32_t main() {
-    slow;
-    int tc;
-    cin >> tc;
-    while (tc--) {
+void solve(){
+    int n,m; cin >> n >> m;
+    vector<string>s(n);
+    for(auto &u : s) cin >> u;
+    string ans;
+    for(int i = 0; i<m; i++){
+        for(int j = 0; j<n; j++){
+            if(s[j][i] == 'v' and ans.empty()){
+                ans += s[j][i];
+                break;
+            }
+            else if(s[j][i] == 'i' and ans.back() == 'v'){
+                ans += s[j][i];
+                break;
+            }
+            else if(s[j][i] == 'k' and ans.back() == 'i'){
+                ans += s[j][i];
+                break;
+            }
+            else if(s[j][i] == 'a' and ans.back() == 'k'){
+                ans += s[j][i];
+                break;
+            }
+        }
+    }
+    //cout<<ans<<" ";
+    if(ans == "vika") cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+}
+int main(){
+	optimize();
+    int tc = 1; cin >> tc;
+    while(tc--){
         solve();
     }
-    return 0;
 }

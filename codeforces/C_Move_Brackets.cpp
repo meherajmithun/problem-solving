1140c682ebe8840f76aba7cbb224c9824489faa7
meherajmithun <meherajhossainmithun@gmail.com>
2025-01-27 18:14:07 +0600

    added

--- codeforces/C_Move_Brackets.cpp
+++ codeforces/C_Move_Brackets.cpp
@@ -1,19 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void slow(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+}
 void solve(){
     int n; cin >> n;
     string s; cin >> s;
-    int cnt=0,res=0;
+    int cnt =0 , ans = 0;
     for(int i=0; i<n; i++){
         if(s[i] == '(') cnt++;
         else if(s[i] == ')' and cnt > 0) cnt--;
-        else res++;
+        else ans++;
     }
-    cout<<res<<endl;
+    cout<<ans<<endl;
 }
-
 int main(){
+    slow();
     int tc; cin >> tc;
     while(tc--){
         solve();

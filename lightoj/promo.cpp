e55415648db1b6524ee97f6244683fc208b29075
meherajmithun <meherajhossainmithun@gmail.com>
2025-02-17 00:06:48 +0600

    Added solved problem

--- lightoj/promo.cpp
+++ lightoj/promo.cpp
@@ -1,29 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define endl '\n'
-#define int long long
 
-void optimize(){
-	ios_base::sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
-}
-
-int32_t main(){
-	optimize();
-	int n,query;
-    cin >> n >> query;
-    vector<int>price(n);
-    for(auto &u : price) cin >> u;
-    sort(price.begin() , price.end());
-    reverse(price.begin() , price.end());
-    vector<int>pre(n+1);
-    for(int i=1; i<=n; i++){
-        pre[i] = pre[i-1] + price[i-1];
+int main(){
+    string s,s2;
+    cin  >> s >> s2;
+    int cost = 0;
+    int line_number = 0;
+    int mx = s.size()-s2.size()+1;
+    for(int i=0; i<mx; i++){
+        line_number++;
+        bool paisi = true;
+        for(int j=0; j<(int)s2.size() and paisi; j++){
+            if(s[i+j] != s2[j]){
+                paisi = false;
+            }
+            cost++;
+        }
+        if(paisi){
+            cout<<line_number<<" ";
+            cout<<cost<<endl;
+            return 0;
+        }
     }
-    // for(auto u : pre) cout<<u<<" ";
-    //     cout<<endl;
-    while(query--){
-        int x,y; cin >> x >> y;
-        cout<<(pre[x] - pre[x-y])<<endl;
-    }
+    cout<<cost<<endl;
 }

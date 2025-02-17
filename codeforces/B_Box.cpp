2909ae28f1afcced7293027a3aa0604b2bb610b0
meherajmithun <meherajhossainmithun@gmail.com>
2025-02-17 16:31:03 +0600

    Added new learned topic

--- codeforces/B_Box.cpp
+++ codeforces/B_Box.cpp
@@ -7,7 +7,7 @@ void slow(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
 }
-
+            
 void solve(){
     int n; cin >> n;
     vector<int>v(n+1);
--- lightoj/test.cpp
+++ lightoj/test.cpp
@@ -1,26 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define print(x) cout<< x << '\n'
 
-void slow(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);cout.tie(0);
-}
-
-void solve(){
-    int n; cin >> n;
-    string s; cin >> s;
-    int cnt = 0;
-    if(s[0] == '1') cnt++;
-    for(int i=1; i<n; i++){
-        if(s[i] != s[i-1]) cnt++;
-    }
-    print(cnt);
-}
 int main(){
-    slow();
-    int tc; cin >> tc;
-    while(tc--){
-        solve();
+    int n,k;
+    cin >> n >> k;
+    vector<int>v(n+1);
+    for(int i=1; i<=n; i++) cin >> v[i];
+    multiset<int>ms;
+    for(int i=1; i<=k; i++){
+        ms.insert(v[i]);
+    }
+
+    for(int i=1; i<=(n-k+1); i++){
+        int mn = *ms.begin();
+        int mx = *ms.rbegin();
+        cout<<mn<<" "<<mx<<'\n';
+        if((i+k) <= n){
+            ms.erase(ms.find(v[i]));
+            ms.insert(v[i+k]);
+        }
     }
 }

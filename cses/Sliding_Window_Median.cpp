491de77a568bad7de3ca33887f446996ef53ffc8
meherajmithun <meherajhossainmithun@gmail.com>
2025-05-03 20:18:52 +0600

    added solution

--- cses/Sliding_Window_Median.cpp
+++ cses/Sliding_Window_Median.cpp
@@ -5,28 +5,71 @@ using namespace std;
 #define int long long
 #define endl '\n'
 #define print(x) cout<<x<<'\n'
+#define INT_MAX LLONG_MAX
+#define INT_MIN LLONG_MIN
 #define mod 1000000007
+multiset<int>ms1,ms2;
+
+void balance(){
+    int sz = ms1.size()+ms2.size();
+    int sz1 = (sz+1)/2;
+    int sz2 = sz-sz1;
+    while(ms1.size()>sz1){
+        int val = *ms1.rbegin();
+        ms2.insert(val);
+        ms1.erase(ms1.find(val));
+    }
+    while(ms2.size()>sz2){
+        int val = *ms2.begin();
+        ms1.insert(val);
+        ms2.erase(ms2.find(val));
+    }
+}
+
+void add(int n){
+    if(ms1.empty()){
+        ms1.insert(n);
+    }
+    else{
+        int val = *(ms1.rbegin());
+        if(val<n){
+            ms2.insert(n);
+        }
+        else{
+            ms1.insert(n);
+        }
+    }
+    balance();
+}
+
+void remove(int n){
+    if(ms1.find(n) != ms1.end()){
+        ms1.erase(ms1.find(n));
+    }
+    else{
+        ms2.erase(ms2.find(n));
+    }
+    balance();
+}
 
 void solve() {
     int n,k; cin>>n>>k;
-    vector<int>v(n+1);
-    for(int i=1; i<=n; i++) cin>>v[i];
-    //sort(v.begin(),v.end());
-    vector<int>tmp = v;
-    int l=1,r=k;
-    while(r<=n){
-        int mid = (l+r)/2;
-        sort(v.begin()+l , v.begin()+r+1);
-        for(int i=l;i<=r; i++) cout<<v[i]<<" ";
-        cout<<endl;
-        if(k%2 == 0){
-            cout<<min(v[mid],v[mid-1])<<" ";
-        }
-        else{
-            cout<<v[mid]<<" ";
-        }
-        r++;
-        l++;
+    vector<int>v(n);
+    for(auto &u : v) cin>>u;
+
+    // for(auto u : v) cout<<u<<" ";
+    // cout<<endl;
+    for(int i=0; i<k; i++){
+        add(v[i]);
+    }
+    cout<<*ms1.rbegin()<<" ";
+    for(int i=k; i<n; i++){
+        remove(v[i-k]);
+        
+        add(v[i]);
+        cout<<*ms1.rbegin()<<" ";
+        
+        // else break;
     }
 
 }

585190b3c044607e71c6b9c558ac499f9e230ecc
meherajmithun <meherajhossainmithun@gmail.com>
2025-07-28 01:34:54 +0600

    add cses solution

---
+++ cses/Creating_Strings.cpp
@@ -0,0 +1,47 @@
+//  IN THE NAME OF ALLAH
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl '\n'
+#define print(x) cout << x << '\n'
+#define mod 1000000007
+int dx[] = {0, 0, +1, -1};
+int dy[] = {+1, -1, 0, 0};
+// int dx[] = {+1, 0, -1, 0, +1, +1, -1, -1};
+// int dy[] = {0, +1, 0, -1, +1, -1, +1, -1};
+string s;
+int n;
+set<string>ans;
+
+void check(int k){
+    if(k == s.size()){
+        ans.insert(s);
+        return;
+    }
+    for(int i=k; i<n; i++){
+        swap(s[k],s[i]);
+        check(k+1);
+        swap(s[k],s[i]);
+    }
+}
+
+void solve() {
+    cin>>s;
+    n = s.size();
+    sort(s.begin() , s.end());
+    check(0);
+    cout<<ans.size()<<endl;
+    for(auto u : ans) cout<<u<<endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int tc = 1; //cin >> tc;
+    while (tc--) {
+        solve();
+    }
+    return 0;
+}

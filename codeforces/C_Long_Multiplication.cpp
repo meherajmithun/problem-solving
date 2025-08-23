b24169bf4dcf2efa37e440abebfe6be0a8d58a63
meherajmithun <meherajhossainmithun@gmail.com>
2025-08-23 13:47:12 +0600

    add

--- codeforces/C_Long_Multiplication.cpp
+++ codeforces/C_Long_Multiplication.cpp
@@ -1,30 +1,55 @@
 #include<bits/stdc++.h>
+#define int long long
+#define endl '\n'
 using namespace std;
 
-int main(){
-    int tc; cin >> tc;
-    while(tc--){
-        string s, s2;
-        cin >> s >> s2;
-        int n = (int)s.size();
-        int aiche = true;
-        for(int i=0; i<n; i++){
-
-            if(!aiche){
-                if(s[i] > s2[i]){
-                    swap(s[i] , s2[i]);
-                }
-            }
-            else {
-                if(s[i] < s2[i]){
-                    swap(s[i] , s2[i]);
-                }
-                aiche = false;
-                
+void solve(){
+    string s,s2;
+    cin>>s>>s2;
+    string s3 = s;
+    string s4 = s2;
+    int n = s.size();
+    int ans,ans2;
+    bool aiche = 1;
+    for(int i=0; i<n; i++){
+        if(s[i]>s2[i] and aiche){
+            swap(s[i],s2[i]);
+            aiche = 0;
+            continue;
+        }
+        if(!aiche){
+            if(s[i]<s2[i]){
+                swap(s[i],s2[i]);
             }
         }
+    }
+    aiche = 1;
+    for(int i=0; i<n; i++){
+        if(s[i]<s2[i] and aiche){
+            swap(s[i],s2[i]);
+            aiche = 0;
+            continue;
+        }
+        if(!aiche){
+            if(s[i]>s2[i]){
+                swap(s[i],s2[i]);
+            }
+        }
+    }
+    //cout<<s<<" "<<s2<<" "<<s3<<" "<<s4<<endl;
+    // cout<<max(s,s3)<<endl<<max(s2,s4)<<endl;
+
+    cout<<s<<endl<<s2<<endl;
+
+}
+
+
+int32_t main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
     
-        cout<<s<<'\n'<<s2<<'\n';
-    
+    int tc; cin>>tc;
+    while(tc--){
+        solve();
     }
 }

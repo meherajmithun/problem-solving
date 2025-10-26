87781503a00fc58d40f9619d9bdd6dc805ae29f5
meherajmithun <meherajhossainmithun@gmail.com>
2025-10-26 23:23:19 +0600

    add

--- codeforces/B_Zero_Array.cpp
+++ codeforces/B_Zero_Array.cpp
@@ -1,32 +1,22 @@
-//  IN THE NAME OF ALLAH
- 
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
-#define endl '\n'
-#define print(x) cout<<x<<'\n'
-#define mod 1000000007
-// int modulo(int a,int b){
-//     if(b == 0) return 1;
-//     int k = modulo(a,b/2);
-//     if(b&1){
-//         return (a*((k*k)%mod))%mod;
-//     }
-//     return (k*k)%mod;
-// }
 
-int32_t main(){
-    int n; cin>>n;
-    vector<int>v(n);
+int32_t main()
+{
+    int n;
+    cin >> n;
+    vector<int> cs(n);
+    for (auto &a : cs)
+        cin >> a;
+    sort(cs.begin(), cs.end());
     int sum = 0;
-    for(int i=0; i<n; i++){
-        int a; cin>>a;
+    for (auto a : cs)
         sum += a;
+    if ((sum % 2 == 0) and (cs[n - 1]) <= (sum - cs[n - 1]))
+    {
+        cout << "YES";
     }
-    if(sum&1){
-        cout<<"NO\n";
-    }
-    else{
-        cout<<"YES\n";
-    }
+    else
+        cout << "NO\n";
 }
--- codeforces/C_Strongly_Composite.cpp
+++ codeforces/C_Strongly_Composite.cpp
@@ -1,85 +1,63 @@
-//  IN THE NAME OF ALLAH
-
-#include <bits/stdc++.h>
-#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define int long long
-#define endl '\n'
-#define nl '\n'
-#define sz(x) ((int)(x).size())
-#define all(x) (x).begin(), (x).end()
-#define rall(x) (x).end(), (x).begin()
-#define print(x) cout<<x<<'\n'
+#include<bits/stdc++.h>
 using namespace std;
-const ll N = (ll) 3e7;
-bitset<N+123> isprime;
-// isprime[i] = 1 --> prime
-// isprime[i] = 0 --> Not prime
+#define ll long long
+#define int long long
 
-void sieve() {
-    for (int i = 3; i <= N; i += 2) {
-        isprime[i] = 1;
-    }
-    isprime[2] = 1;
+const ll N = (ll) 3e6+123;
+bitset<N>isprime;
+vector<int>prime;
+map<int,int>mp;
 
-    for (int i = 3; i <= sqrt(N); i += 2) {
-        if (isprime[i]) {
-            for (int j = (i * i); j <= sqrt(N); j += (2 * i)) {
-                isprime[j] = 0;
-            }
-        }
-    }
-    vector<int> prime;
-    prime.push_back(2);
-    for (int i = 3; i <= N; i += 2) {
-        if (isprime[i]) {
-            prime.push_back(i);
-        }
-    }
-    // for (auto u : prime) cout << u << " ";
+void sieve(){
+	for(int i=3; i<=N; i+=2) isprime[i]=1;
+	isprime[2]=1;
+	for(int i=3; i<=sqrt(N); i+=2){
+		if(isprime[i])
+		for(int j=i*i; j<=N; j+=(2*i)){
+			isprime[j]=0;		
+		}
+	}
+	prime.push_back(2);
+	for(int i=3; i<=N; i+=2){
+		if(isprime[i]) prime.push_back(i);
+	}
+	//for(int i=0; i<100; i++) cout<<prime[i]<<" ";
 }
 
-vector<int>divisor(int n){
-    vector<int>div;
-    for(int i=2; i<=sqrt(n); i++){
-        if(n%i == 0){
-            int a = i, b= n/i;
-            int cnt = 0,cnt1=0;
-            if(isprime[a]) {cnt++, k++;}
-            else {cnt1++ , k++;}
-            for(int j=2; j<=sqrt(a); j++){
-                if(n%j == 0){
-                    k++;
-                    if(isprime[j]) cnt++;
-                    else cnt1++;
-                    if(n/i != i){
-                        k++;
-                        if(isprime[n/i]) cnt++;
-                        else cnt1++;
-                    }
-                }
-            }
-            if(a == b){
-                if()
-            }
-        }
-    }
+void prime_factor(int n){
+	for(int i=0; prime[i]*prime[i]<=n; i++){
+		while(n%prime[i] == 0){
+			n /= prime[i];
+			mp[prime[i]]++;
+		}
+	}
+	if(n>1) mp[n]++;
+	//for(auto [a,b] : mp) cout<<a<<"->"<<b<<endl;
 }
 
-void solve() {
-    int n; cin>>n;
-    vector<int>v(n);
-    for(auto &u : v) cin>>u;
-    int k = 1;
-    for(auto u : v) k *= u;
-    divisor(k);
+void solve(){
+	int n; cin>>n;
+	for(int i=0; i<n; i++){
+		int a; cin>>a;
+		prime_factor(a);
+	}
+	int ans = 0,cnt=0;
+	for(auto [a,b] : mp){
+	//	cout<<a<<"->"<<b<<endl;
+		ans += (b/2);
+		cnt += (b%2);
+	}
+	//cout<<ans<<" ";
+	ans += (cnt/3);
+	mp.clear();
+	cout<<ans<<'\n';
+
 }
 
-int32_t main() {
-    fast
-    sieve();
-    int tc = 1; cin >> tc;
-    while (tc--) {
-        solve();
-    }
-    return 0;
+int32_t main(){
+	sieve();
+	int tc; cin>>tc;
+	while(tc--){
+		solve();
+	}
 }
